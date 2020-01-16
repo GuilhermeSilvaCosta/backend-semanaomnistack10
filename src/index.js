3f88f73b132bc1f19e98d220d0e92b38ac4d9825
Guilherme Costa <tec.guilherme.dsc@gmail.com>
2020-01-16 20:12:48 -0300

    Conclusão backend

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-ygfip.mongodb.net/week10?retryWrites=true&w=majority',
+{ useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.get('/', (req, res) => {
+    return res.json({ message: 'hello omnistack'});
+})
+
+app.listen(3333);
