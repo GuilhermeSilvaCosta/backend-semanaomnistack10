0d1bc6d94f2f142aee56748baa9129e5ef4fc77b
Guilherme Costa <tec.guilherme.dsc@gmail.com>
2020-04-28 17:07:07 -0300

    Commit on cloud

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -11,15 +12,15 @@ const server = http.Server(app);
 setupWebsocket(server);
 
 mongoose.connect(process.env.MONGO_URL,
-{ useNewUrlParser: true, useUnifiedTopology: true });
+{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use('/week10', routes);
 
 app.get('/', (req, res) => {
     return res.json({ message: 'hello omnistack'});
     
 })
 
-server.listen(3333);
+server.listen(process.env.PORT);
