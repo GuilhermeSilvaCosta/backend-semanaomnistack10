e9b9800e4696c4e7cda7b555e7a1b46e531a48e8
Guilherme Costa <tec.guilherme.dsc@gmail.com>
2020-04-28 17:31:44 -0300

    Disable cors

--- src/index.js
+++ src/index.js
@@ -14,13 +14,8 @@ setupWebsocket(server);
 mongoose.connect(process.env.MONGO_URL,
 { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 app.use('/week10', routes);
-
-app.get('/', (req, res) => {
-    return res.json({ message: 'hello omnistack'});
-    
-})
 
 server.listen(process.env.PORT);
